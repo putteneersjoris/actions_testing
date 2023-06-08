d63ba7525e185d1f0b28fab63eaae9fb5b127935
putteneersjoris <putteneersjoris@gmail.com>
2023-06-08 02:17:59 +0200

    t

--- github_actions.py
+++ github_actions.py
@@ -10,15 +10,18 @@ with open('text.txt', 'r') as file:
 time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 generated_text = f"generated with python on {time}\n{content}"
 
-# Save the generated text to 'actions.txt'
-with open('actions.txt', 'w') as file:
-    file.write(generated_text)
-
-# Check if 'actions.txt' exists and print its path if it does, or print "file not found" if it doesn't
-if os.path.exists('blabla.txt'):
-    file_path = os.path.abspath('actions.txt')
-    print("Path of 'actions.txt':", file_path)
+# Save the generated text to 'actions.txt' or 'actions_testing.txt'
+if os.path.exists('actions.txt'):
+    file_path = 'actions.txt'
 else:
-    print("File not found.")
+    file_path = 'actions_testing.txt'
+    with open(file_path, 'w') as file:
+        file.write(generated_text)
+    print(f"Created new file: {file_path}")
+
+# Print the content of the file
+with open(file_path, 'r') as file:
+    file_content = file.read()
+    print(file_content)
 
 print("Script executed successfully.")
